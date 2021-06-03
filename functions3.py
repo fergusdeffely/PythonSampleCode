a1578d9293d67e770a6daabdf95f82361d740836
Fergus Deffely <fergdeff@gmail.com>
2021-06-03 17:22:54 +0100

    new file:   exceptions1.py  new file:   factorial-program-flow.txt  new file:   factorial.py  new file:   functions1.py  new file:   functions2.py  new file:   functions3.py
    Adding sample code to give examples of functions, recursion and exception handling

---
+++ functions3.py
@@ -0,0 +1,10 @@
+def print_pattern(num_rows):
+    for i in range(num_rows):
+        print("*" * (num_rows - i))
+    print()
+        
+print_pattern(5)
+print_pattern(3)
+
+# TODO: Create new function with two params - one for character, 
+#       one for num rows
