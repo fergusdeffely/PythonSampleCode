52b8c058cd5d61efd69315fc550b6848e81dc40c
Fergus Deffely <fergdeff@gmail.com>
2021-06-08 15:19:07 +0100

    Added additional logic to checking for number of seconds
    modified:   if-else.py
     - Added additional logic to checking for number of seconds

--- if-else.py
+++ if-else.py
@@ -2,11 +2,14 @@
 # Assign a numeric value
 seconds = 40
 
-# Adding another print statement
-print("Possibly unnecessary")
-
 # Check if the number of seconds is less than or equal to 60 (one minute)
 if (seconds < 61):
     print("Under one minute")
+    print("That is: less than 60 seconds!")
+
+    if (seconds <= 30):
+        print("In fact - less than half a minute!")
 else:
     print("More than one minute")
+
+print("And that is all...")
