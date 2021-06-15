cb6165c29d11cbbb70446fec54c4929525315769
Fergus Deffely <fergdeff@gmail.com>
2021-06-15 16:17:54 +0100

    Add simple title search functionality
     modified:   dicts-as-objects.py
      - Add simple title search functionality

--- dicts-as-objects.py
+++ dicts-as-objects.py
@@ -8,5 +8,9 @@ booklist.append({"title": "Green Eggs and Ham", "author": "Dr. Suess", "onloan":
 booklist.append({"title": "Goodnight Moon", "author": "Margaret Wise Brown", "onloan": False})
 booklist.append({"title": "Charlotte's Web", "author": "E.B. White", "onloan": False})
 
+searchTerm = "Are"
+
 for book in booklist:
     print(f"{book['title'].ljust(30)} {book['author'].ljust(20)} {str(book['onloan'])}")
+    if(book['title'].find(searchTerm) != -1):
+        print(f"    Contains '{searchTerm}'")
