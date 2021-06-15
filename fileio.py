57858b0814dbede96e9900630462686e93c8400c
Fergus Deffely <fergdeff@gmail.com>
2021-06-15 12:08:29 +0100

    Added comments on file open modes
     modified:   fileio.py
      - Added comments on file open modes

--- fileio.py
+++ fileio.py
@@ -1,9 +1,17 @@
 # Open a file
 print("Opening foo.txt for writing...")
 
+# second param is 'mode'
+#  w = write (overwrite current contents)
+#  a = append (write, appending to current contents)
+#  r = read (read from file)
+
 fo = open("foo.txt", "w")
-fo.write( "1. write:\nHere is some text.\nAnd here is some more text!!\n\n")
-fo.writelines(["2. writelines\n", "Here is some text\n", "And here is some more text!!!\n"])
+fo.write( "1. write:\nHere is some text\nAnd here is some more text!!\n\n")
+stringlist = ["2. writelines\n", "Here is some text\n", "And here is some more text!!!\n"]
+fo.writelines(stringlist)
+#fo.writelines(["2. writelines\n", "Here is some text\n", "And here is some more text!!!\n"])
+
 fo.close()
 
 print("Opening foo.txt for reading...")
